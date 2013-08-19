035266c3f469456ba6353c48a4a97a10c6989c52
Mikolai Fajer <mfajer@gmail.com>
2013-08-18 21:11:41 -0500

    Further revision on the heatmap, this time with Gaussian blurring.

--- bin/hist_navi.py
+++ bin/hist_navi.py
@@ -103,6 +103,7 @@ with io.open(path, 'r+b') as infile:
 import pylab
 import matplotlib
 import matplotlib.image
+import scipy.ndimage
 background_map = matplotlib.image.imread('dota_map.png')
 # Grayscale the image so the contourf shows up more clearly
 def rgb2gray(rgb):
@@ -116,19 +117,25 @@ for (x, y) in zip(navi_xs, navi_ys):
 	mx, my = mapper.to_mapped(x, y)
 	mapped_xs.append(mx)
 	mapped_ys.append(my)
-orange_alpha = matplotlib.colors.LinearSegmentedColormap('OrangeAlpha', {'red': ((0.0, 1.0, 1.0), (0.5, 1.0, 1.0), (1.0, 1.0, 1.0)),
-									 'green': ((0.0, 0.96, 0.96), (0.5, 0.82, 0.82), (1.0, 0.55, 0.55)),
-									 'blue': ((0.0, 0.92, 0.92), (0.5, 0.63, 0.63), (1.0, 0.23, 0.23)),
-									 'alpha': ((0.0, 0.0, 0.0), (0.05, 0.0, 0.0), (1.0, 1.0, 1.0))})
-H, xedges, yedges = numpy.histogram2d(mapped_xs, mapped_ys, bins=100)
+cmap_with_alpha = matplotlib.colors.LinearSegmentedColormap('OrangeRedAlpha', {'red': ((0.0, 1.0, 1.0), (1.0, 0.5, 0.5)),
+								'green': ((0.0, 0.55, 0.55), (1.0, 0.15, 0.15)),
+								'blue': ((0.0, 0.23, 0.23), (1.0, 0.0, 0.0)),
+								'alpha': ((0.0, 0.0, 0.0), (0.05, 0.0, 0.0), (0.25, 0.5, 0.5), (1.0, 1.0, 1.0))})
+# Do a pixel-wide histogram followed by a strong Gaussian blur
+xedges = numpy.arange(0, background_map.shape[0], 1)
+yedges = numpy.arange(0, background_map.shape[1], 1)
+H, xedges, yedges = numpy.histogram2d(mapped_xs, mapped_ys, bins=(xedges, yedges))
+H = scipy.ndimage.gaussian_filter(H, sigma=50)
 X, Y = 0.5*(xedges[1:]+xedges[:-1]), 0.5*(yedges[1:]+yedges[:-1])
 # Re-orient so the (0,0) is in the radiant corner
 pylab.imshow(background_map[::-1, :], origin='lower', cmap=pylab.cm.gray)
-pylab.contourf(X, Y, H, cmap=orange_alpha)
+#pylab.contourf(X, Y, H.transpose(), 10, cmap=cmap_with_alpha)
+pylab.contourf(X, Y, numpy.log10(H.transpose()+1), 10, cmap=cmap_with_alpha)
 pylab.xlim(0, background_map.shape[1])
 pylab.ylim(0, background_map.shape[0])
 pylab.gca().get_xaxis().set_visible(False)
 pylab.gca().get_yaxis().set_visible(False)
+pylab.tight_layout(0)
 pylab.savefig('mapped_hist.png')
 pylab.close()
 
