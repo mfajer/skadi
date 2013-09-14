b774dcf6dafc36fc1c0e7e56d0e6494c1e28bfa1
Mikolai Fajer <mfajer@gmail.com>
2013-09-14 15:09:19 -0500

    Tracking kotl's position and health.
    Conflicts:
    
    	bin/skadi

--- skadi/util/mapping.py
+++ skadi/util/mapping.py
@@ -7,12 +7,16 @@ class CoordinateMapper(object):
 		'''Pass a reference dictionary of entity_name: {'x':x, 'y':y} coordinates.'''
 		self._reference = copy.deepcopy(reference)
 		# Add the cell coordinates into the reference
+		remove = []
 		for key, val in self._reference.iteritems():
 			matching_entities = filter(lambda x: 'DT_BaseEntity.m_iName' in x.state and x.state['DT_BaseEntity.m_iName'] == key, snapshot.instances.itervalues())
 			if len(matching_entities) != 1:
-				raise UserWarning("Incorrect number of matches (%d)" % len(matching_entities))
-			val['cellX'] = matching_entities[0].state['DT_DOTA_BaseNPC.m_cellX']
-			val['cellY'] = matching_entities[0].state['DT_DOTA_BaseNPC.m_cellY']
+				remove.append(key)
+			else:
+				val['cellX'] = matching_entities[0].state['DT_DOTA_BaseNPC.m_cellX']
+				val['cellY'] = matching_entities[0].state['DT_DOTA_BaseNPC.m_cellY']
+		for key in remove:
+			del self._reference[key]
 		self._generate_mapping()
 
 	def _generate_mapping(self):
@@ -53,10 +57,11 @@ if __name__ == "__main__":
 	# Set up the backend for display-less plotting	
 	import matplotlib
 	matplotlib.rcParams['backend'] = 'Agg'
+	sys.exit()
 
 	# Load the background map
 	import matplotlib.image
-	background_map = matplotlib.image.imread('../../dota_map.png')
+	background_map = matplotlib.image.imread('dota_map.png')
 
 	# Plot the least-squares fitting for the mapping
 	import pylab
@@ -80,12 +85,50 @@ if __name__ == "__main__":
 		mapped_xs.append(mx)
 		# Re-orient so the (0,0) is in the radiant corner
 		mapped_ys.append(background_map.shape[0] - my)
-	# Re-orient so the (0,0) is in the radiant corner
-	pylab.imshow(background_map[::-1, :, :])
-	pylab.plot(mapped_xs, mapped_ys, 'bo')
-	pylab.xlim(0, background_map.shape[1])
-	pylab.ylim(0, background_map.shape[0])
-	pylab.gca().get_xaxis().set_visible(False)
-	pylab.gca().get_yaxis().set_visible(False)
-	pylab.savefig('mapped_pos.png')
-	pylab.close()
+#	# Re-orient so the (0,0) is in the radiant corner
+#	pylab.imshow(background_map[::-1, :, :])
+#	pylab.plot(mapped_xs, mapped_ys, 'b')
+#	pylab.xlim(0, background_map.shape[1])
+#	pylab.ylim(0, background_map.shape[0])
+#	pylab.gca().get_xaxis().set_visible(False)
+#	pylab.gca().get_yaxis().set_visible(False)
+#	pylab.savefig('mapped_pos.png')
+#	pylab.close()
+
+
+	# Plot the position of the of the hero, colored by health
+	# Create a set of line segments so that we can color them individually
+	# This creates the points as a N x 1 x 2 array so that we can stack points
+	# together easily to get the segments. The segments array for line collection
+	# needs to be numlines x points per line x 2 (x and y)
+	points = numpy.array([mapped_xs, mapped_ys]).T.reshape(-1, 1, 2)
+	segments = numpy.concatenate([points[:-1], points[1:]], axis=1)
+
+	from matplotlib.collections import LineCollection
+	window = 1800
+	start_indices = range(0, len(segments)-window, window/4)
+	stop_indices = range(window, len(segments), window/4)
+	for image_idx, (start_idx, stop_idx) in enumerate(zip(start_indices, stop_indices)):
+		# Plot the background image
+		pylab.imshow(background_map[::-1, :, :])
+
+		# Create the line collection object, setting the colormapping parameters.
+		# Have to set the actual values used for colormapping separately.
+		lc = LineCollection(segments[start_idx:stop_idx], cmap=pylab.get_cmap('jet_r'),
+			norm=pylab.Normalize(0, 1))
+		lc.set_array(numpy.array(frac_health[start_idx:stop_idx]))
+		lc.set_linewidth(2)
+		pylab.gca().add_collection(lc)
+
+#		# Plot the finger positions
+#		ult_indices = numpy.where(numpy.diff(ult_cd) > 0)[0]
+#		pylab.plot([mapped_xs[idx] for idx in ult_indices], [mapped_ys[idx] for idx in ult_indices], 'ro')
+
+		# Finalize the figure
+		pylab.xlim(0, background_map.shape[1])
+		pylab.ylim(0, background_map.shape[0])
+		pylab.gca().get_xaxis().set_visible(False)
+		pylab.gca().get_yaxis().set_visible(False)
+		pylab.colorbar(lc)
+		pylab.savefig('mapped_pos_%04d.png' % image_idx)
+		pylab.clf()
